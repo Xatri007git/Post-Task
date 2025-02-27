2eb747bbd58990cdbbefd685b4309f8d7b71d457
Atri <chaudhuriatri@gmail.com>
2025-02-27 22:51:01 +0530

    Flags added

--- Task1_new.go
+++ Task1_new.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"path/filepath"
 
 	"github.com/xuri/excelize/v2"
 
@@ -9,6 +12,7 @@ import (
 
 	"os"
 
+	"math"
 	"sort"
 )
 
@@ -21,6 +25,9 @@ var compre_avg float64
 var total_avg float64
 var total_avg_2024 map[string]float64
 var n_students_2024 map[string]int
+var class int64
+var js string
+var e float64 // margin of error for addition
 
 type Student struct {
 	Emplid string
@@ -31,6 +38,62 @@ type Student struct {
 	weeklab_marks float64
 	compre_marks  float64
 	total_marks   float64
+}
+
+type Report struct {
+	Errors          []string `json:"Errors"`
+	Component_avgs  []string `json:"Component Averages"`
+	Branch_avg_2024 []string `json:"Branchwise average 2024 batch"`
+	Rank_quiz       []string `json:"Rank holders for quiz"`
+	Rank_midsem     []string `json:"Rank holders for midsem"`
+	Rank_labtest    []string `json:"Rank holders for labtest"`
+	Rank_weekly_lab []string `json:"Rank holders for weekly labs"`
+	Rank_compre     []string `json:"Rank holders for compre"`
+	Rank_overall    []string `json:"Rank holders for overall total"`
+}
+
+func create_json(errors []string, comp_avs []string, branch_avs []string,
+	rank_q []string, rank_m []string, rank_l []string, rank_wl []string, rank_c []string, rank_o []string, filepath string) {
+	report := Report{errors, comp_avs, branch_avs, rank_q, rank_m, rank_l, rank_wl, rank_c, rank_o}
+	loc := []Report{report}
+
+	finaljson, err := json.MarshalIndent(loc, "", "\t")
+	if err != nil {
+		fmt.Println("Could not create json file")
+		os.Exit(1)
+	}
+
+	writefile := os.WriteFile(filepath, finaljson, 0644)
+	if writefile != nil {
+		fmt.Println("Error in writing json file")
+		os.Exit(1)
+	}
+
+	fmt.Println("The json file is displayed below")
+	fmt.Printf("%s\n", finaljson)
+	fmt.Printf("The above json file has been exported at %s", filepath)
+}
+
+func read_flags() {
+	flag.Int64Var(&class, "class", -1, "class No. of the student")
+	flag.StringVar(&js, "export", "", "export to json")
+	flag.Parse()
+}
+
+func class_fliter(row []string) bool {
+	if class == -1 {
+		return true // no filter
+	}
+
+	return strInt(row[1]) == class
+}
+
+func export_filter() bool {
+	if js != "json" && js != "" {
+		fmt.Println("Invalid value for export flag")
+		os.Exit(1)
+	}
+	return js == "json"
 }
 
 func display(x Student, rank int, component string) {
@@ -55,18 +118,29 @@ func display(x Student, rank int, component string) {
 
 func add_student(all_students *[]Student, row []string) {
 	emplid := row[2]
-	quiz := strint(row[4])
-	midsem := strint(row[5])
-	labtest := strint(row[6])
-	weeklab := strint(row[7])
-	compre := strint(row[9])
+	quiz := strfloat(row[4])
+	midsem := strfloat(row[5])
+	labtest := strfloat(row[6])
+	weeklab := strfloat(row[7])
+	compre := strfloat(row[9])
 	total := quiz + midsem + labtest + weeklab + compre
 	*all_students = append(*all_students, Student{emplid, quiz, midsem, labtest, weeklab, compre, total})
 }
 
-func strint(x string) float64 {
+func strfloat(x string) float64 {
 
 	y, err := strconv.ParseFloat(x, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return y
+}
+
+func strInt(x string) int64 {
+
+	y, err := strconv.ParseInt(x, 10, 64)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -79,11 +153,11 @@ func branch_wise_avg(row []string) {
 	id := row[3]
 	year := id[:4]
 
-	quiz := strint(row[4])
-	midsem := strint(row[5])
-	labtest := strint(row[6])
-	weeklab := strint(row[7])
-	compre := strint(row[9])
+	quiz := strfloat(row[4])
+	midsem := strfloat(row[5])
+	labtest := strfloat(row[6])
+	weeklab := strfloat(row[7])
+	compre := strfloat(row[9])
 
 	if year == "2024" {
 		key := id[:6]
@@ -94,52 +168,53 @@ func branch_wise_avg(row []string) {
 
 func calculate_avg(row []string) {
 
-	quiz := strint(row[4])
+	quiz := strfloat(row[4])
 	quiz_avg += quiz
 
-	midsem := strint(row[5])
+	midsem := strfloat(row[5])
 	midsem_avg += midsem
 
-	labtest := strint(row[6])
+	labtest := strfloat(row[6])
 	labtest_avg += labtest
 
-	weeklab := strint(row[7])
+	weeklab := strfloat(row[7])
 	weeklab_avg += weeklab
 
-	compre := strint(row[9])
+	compre := strfloat(row[9])
 	compre_avg += compre
 }
 
 // floating point error
-func validate_row(row []string, rownum int, errors *[]string) bool {
+func validate_row(row []string, errors *[]string) bool {
 
 	good := true
-	quiz := strint(row[4])
+	quiz := strfloat(row[4])
 
-	midsem := strint(row[5])
+	midsem := strfloat(row[5])
 
-	labtest := strint(row[6])
+	labtest := strfloat(row[6])
 
-	weeklab := strint(row[7])
+	weeklab := strfloat(row[7])
 
-	pct := strint(row[8])
+	pct := strfloat(row[8])
 
-	compre := strint(row[9])
+	compre := strfloat(row[9])
 
-	total := strint(row[10])
+	total := strfloat(row[10])
 
-	if quiz+midsem+labtest+weeklab != pct {
-		*errors = append(*errors, fmt.Sprintf("%s%d%s%s", "Incorrect pct calculation in row ", rownum, " with Sl No. ", row[0]))
+	if math.Abs(quiz+midsem+labtest+weeklab-pct) > e {
+		*errors = append(*errors, fmt.Sprintf("%s%s", "Incorrect pct calculation in row with Sl No. ", row[0]))
 		good = false
 	}
 
-	if quiz+midsem+labtest+weeklab+compre != total {
-		*errors = append(*errors, fmt.Sprintf("%s%d%s%s", "Incorrect pct calculation in row ", rownum, " with Sl No. ", row[0]))
+	if math.Abs(quiz+midsem+labtest+weeklab+compre-total) > e {
+		*errors = append(*errors, fmt.Sprintf("%s%s", "Incorrect total calculation in row with Sl No. ", row[0]))
 		good = false
 	}
 
 	return good
 }
+
 func is_row_empty(row []string) bool {
 
 	for _, cell := range row {
@@ -150,8 +225,18 @@ func is_row_empty(row []string) bool {
 
 	return true
 }
+
+func display_row(row []string) {
+	for _, col := range row {
+		fmt.Print(col, "\t")
+	}
+	fmt.Println()
+}
+
 func main() {
 
+	read_flags()
+	e = 0.000001
 	n_students_2024 = make(map[string]int)
 	total_avg_2024 = make(map[string]float64)
 	all_students := []Student{}
@@ -165,9 +250,16 @@ func main() {
 	total_avg = 0
 
 	rownum := 1
-	error_rows := []int{} // stores the row number which has an error
-	errors := []string{}  // stores the errors at a row if any
+	error_rows := []string{} // stores the row number which has an error
+	errors := []string{}     // stores the errors at a row if any
 	empty_rows := []string{}
+	branch_avg_2024 := []string{}
+	rank_quiz := []string{}
+	rank_midsem := []string{}
+	rank_labtest := []string{}
+	rank_weekly_lab := []string{}
+	rank_compre := []string{}
+	rank_overall := []string{}
 
 	var fileName string
 	fmt.Println("Enter the path of the data file:")
@@ -177,43 +269,51 @@ func main() {
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	rows, err := f.GetRows("CSF111_202425_01_GradeBook")
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	fmt.Println("The spreadsheet is diaplayed below")
 	for _, row := range rows {
 
-		for _, col := range row {
-			fmt.Print(col, "\t")
-		}
-		fmt.Println()
+		if rownum == 1 {
+			display_row(row) // display the description row
+			rownum++
+		} else {
 
-		if rownum != 1 {
+			if class_fliter(row) {
+				display_row(row)
+				if is_row_empty(row) {
+					empty_rows = append(empty_rows, fmt.Sprintf("Row with Sl No. %s is empty", row[0]))
+					rownum++
+					continue
+				} // check for empty rows
 
-			if is_row_empty(row) {
-				empty_rows = append(empty_rows, fmt.Sprintf("Row number %d is empty", rownum))
+				if !validate_row(row, &errors) {
+					error_rows = append(error_rows, row[0])
+				}
+
+				calculate_avg(row)
+				branch_wise_avg(row)
+				add_student(&all_students, row)
 				rownum++
-				continue
-			} // check for empty rows
-
-			if !validate_row(row, rownum, &errors) {
-				error_rows = append(error_rows, rownum)
 			}
-
-			calculate_avg(row)
-			branch_wise_avg(row)
-			add_student(&all_students, row)
 		}
 
-		rownum++
 	}
+
+	if rownum == 2 {
+		fmt.Printf("Class flag is invalid, there is no entry in class No. with value %d\n", class)
+		os.Exit(1)
+	}
+
+	fmt.Println()
 
 	quiz_avg /= float64(rownum - 2)
 	midsem_avg /= float64(rownum - 2)
@@ -238,7 +338,7 @@ func main() {
 
 	fmt.Println()
 
-	fmt.Println("The following rows contain errors")
+	fmt.Println("The rows with the following Sl No. contain errors")
 	fmt.Println(error_rows)
 
 	fmt.Println()
@@ -251,18 +351,36 @@ func main() {
 	fmt.Println()
 
 	fmt.Println("The component averages are displayed")
+
+	qa := fmt.Sprintf("The quiz average = %f", quiz_avg)
 	fmt.Printf("The quiz average = %f\n", quiz_avg)
+
+	ma := fmt.Sprintf("The midsem average = %f", midsem_avg)
 	fmt.Printf("The midsem average = %f\n", midsem_avg)
+
+	la := fmt.Sprintf("The labtest average = %f", labtest_avg)
 	fmt.Printf("The labtest average = %f\n", labtest_avg)
+
+	wa := fmt.Sprintf("The weekly lab average = %f", weeklab_avg)
 	fmt.Printf("The weekly lab average = %f\n", weeklab_avg)
+
+	pa := fmt.Sprintf("The pct average = %f", pct_avg)
 	fmt.Printf("The pct average = %f\n", pct_avg)
+
+	ca := fmt.Sprintf("The compre average = %f", compre_avg)
 	fmt.Printf("The compre average = %f\n", compre_avg)
+
+	ta := fmt.Sprintf("The total average = %f", total_avg)
 	fmt.Printf("The total average = %f\n", total_avg)
+
+	Component_avgs := []string{qa, ma, la, wa, pa, ca, ta}
 
 	fmt.Println()
 	fmt.Println("The branch-wise average for the 2024 batch are displayed")
 
 	for key := range total_avg_2024 {
+		s := fmt.Sprintf("Total average for the 2024 %s batch = %f", key[4:6], total_avg_2024[key]/float64(n_students_2024[key]))
+		branch_avg_2024 = append(branch_avg_2024, s)
 		fmt.Printf("The total average for the 2024 %s batch = %f\n", key[4:6], total_avg_2024[key]/float64(n_students_2024[key]))
 	}
 
@@ -273,6 +391,7 @@ func main() {
 	fmt.Println("Rank-holders of quiz")
 	sort.Slice(all_students, func(i, j int) bool { return all_students[i].quiz_marks > all_students[j].quiz_marks })
 	for i := 0; i < 3; i++ {
+		rank_quiz = append(rank_quiz, fmt.Sprintf("%s%s%s%f%s%d", "Emplid = ", all_students[i].Emplid, ", Quiz Marks = ", all_students[i].quiz_marks, ", Rank = ", i+1))
 		display(all_students[i], i+1, "quiz")
 		fmt.Println("-----------------------")
 	}
@@ -281,6 +400,7 @@ func main() {
 	fmt.Println("Rank-holders of midsem")
 	sort.Slice(all_students, func(i, j int) bool { return all_students[i].midsem_marks > all_students[j].midsem_marks })
 	for i := 0; i < 3; i++ {
+		rank_midsem = append(rank_midsem, fmt.Sprintf("%s%s%s%f%s%d", "Emplid = ", all_students[i].Emplid, ", Midsem Marks = ", all_students[i].midsem_marks, ", Rank = ", i+1))
 		display(all_students[i], i+1, "midsem")
 		fmt.Println("-----------------------")
 	}
@@ -289,6 +409,7 @@ func main() {
 	fmt.Println("Rank-holders of labtest")
 	sort.Slice(all_students, func(i, j int) bool { return all_students[i].labtest_marks > all_students[j].labtest_marks })
 	for i := 0; i < 3; i++ {
+		rank_labtest = append(rank_labtest, fmt.Sprintf("%s%s%s%f%s%d", "Emplid = ", all_students[i].Emplid, ", Labtest Marks = ", all_students[i].labtest_marks, ", Rank = ", i+1))
 		display(all_students[i], i+1, "labtest")
 		fmt.Println("-----------------------")
 	}
@@ -297,6 +418,7 @@ func main() {
 	fmt.Println("Rank-holders of weekly labs")
 	sort.Slice(all_students, func(i, j int) bool { return all_students[i].weeklab_marks > all_students[j].weeklab_marks })
 	for i := 0; i < 3; i++ {
+		rank_weekly_lab = append(rank_weekly_lab, fmt.Sprintf("%s%s%s%f%s%d", "Emplid = ", all_students[i].Emplid, ", Weekly lab Marks = ", all_students[i].weeklab_marks, ", Rank = ", i+1))
 		display(all_students[i], i+1, "weekly lab")
 		fmt.Println("-----------------------")
 	}
@@ -305,6 +427,7 @@ func main() {
 	fmt.Println("Rank-holders of compre")
 	sort.Slice(all_students, func(i, j int) bool { return all_students[i].compre_marks > all_students[j].compre_marks })
 	for i := 0; i < 3; i++ {
+		rank_compre = append(rank_compre, fmt.Sprintf("%s%s%s%f%s%d", "Emplid = ", all_students[i].Emplid, ", Compre Marks = ", all_students[i].compre_marks, ", Rank = ", i+1))
 		display(all_students[i], i+1, "compre")
 		fmt.Println("-----------------------")
 	}
@@ -313,7 +436,24 @@ func main() {
 	fmt.Println("Rank-holders of overall total")
 	sort.Slice(all_students, func(i, j int) bool { return all_students[i].total_marks > all_students[j].total_marks })
 	for i := 0; i < 3; i++ {
+		rank_overall = append(rank_overall, fmt.Sprintf("%s%s%s%f%s%d", "Emplid = ", all_students[i].Emplid, ", Total Marks = ", all_students[i].total_marks, ", Rank = ", i+1))
 		display(all_students[i], i+1, "total")
 		fmt.Println("-----------------------")
+	}
+
+	if export_filter() {
+		fmt.Println()
+
+		fmt.Println("Enter the path of directory where you want to save json file")
+
+		fpath := ""
+		fmt.Scanln(&fpath)
+		fname := ""
+
+		fmt.Println("Enter desired file name followed by .json extension")
+		fmt.Scanln(&fname)
+
+		osfpath := filepath.FromSlash(fpath)
+		create_json(errors, Component_avgs, branch_avg_2024, rank_quiz, rank_midsem, rank_labtest, rank_weekly_lab, rank_compre, rank_overall, filepath.Join(osfpath, fname))
 	}
 }
